88ffcec07858aeebe14e74364743e7e9b2b8e59e
Yohan M <inuyasha59260@gmail.com>
2022-04-04 15:54:28 +0200

    YohHomePageFirstVersion

---
+++ components/Layout.tsx
@@ -0,0 +1,29 @@
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+
+export const Layout: React.FC = ({ children }) => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link href="/">Logo Homepage</Link>
+
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+
+            <li className="nav-item" >
+              <Link href="/api/auth/login">
+                <a className="nav-link active"> Login</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div>{children}</div>
+
+    </div>
+  );
+};
