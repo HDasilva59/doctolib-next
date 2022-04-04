3d3924e005947b17a544ec0485a2582afbf95b92
mansuy maxime <mmansuy@norauto.fr>
2022-04-04 18:36:17 +0200

    rectif erreur ancienne pr

--- component/layout.tsx
+++ component/layout.tsx
@@ -1,11 +1,29 @@
-export default function Login(props: {  }) {
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+
+export const Layout: React.FC = ({ children }) => {
   return (
-      <div>
-        <main className="text-center">
-          <h1>Coucou</h1>
-          <h2>Le site est encore en correction !</h2> <br />
-          <h2>Bonne visite !</h2>
-        </main>
-      </div>
-  )
-}
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link href="/">Logo Homepage</Link>
+
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+
+            <li className="nav-item" >
+              <Link href="/login">
+                <a className="nav-link active"> Login</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div>{children}</div>
+
+    </div>
+  );
+};
