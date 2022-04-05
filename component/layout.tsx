bb2504fa0fea28cf09f43f3884fdb6097fe6e586
ZAOUI Sofiane <sofiane.zaoui01@gmail.com>
2022-04-06 01:06:58 +0200

    wip css bootstrap bg and navbar

--- component/layout.tsx
+++ component/layout.tsx
@@ -6,26 +6,30 @@ import Link from "next/link";
 export const Layout: React.FC = ({ children }) => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <nav className="navbar navbar-expand-sm bg-primary navbar-dark">
           <div className="container-fluid">
-          <Link  href="/"><a className="navbar-brand">Home</a></Link>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <Link href="/login">
-                <a className="nav-link active">
-                  <li className="nav-item">Login</li></a>
-              </Link>
-              <Link href="/api/auth/logout">
-                <a className="nav-link active">
-                  <li className="nav-item">Logout</li></a>
-              </Link>
-            </ul>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+            <Link  href="/"><a className="navbar-brand">Home</a></Link>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <Link href="/login">
+                  <a className="nav-link active">
+                    <li className="nav-item">Login</li></a>
+                </Link>
+                <Link href="/api/auth/logout">
+                  <a className="nav-link active">
+                    <li className="nav-item">Logout</li></a>
+                </Link>
+              </ul>
+            </div>
           </div>
-        </div>
       </nav>
 
-
-        {children}
+    <div className="container">
+      {children}
+    </div>
 
 
     </div>
