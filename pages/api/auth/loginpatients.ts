6c8e4aec2009c53877c005834d88593111dd84b9
mansuy maxime <mmansuy@norauto.fr>
2022-04-06 17:08:08 +0200

    maj file

---
+++ pages/api/auth/loginpatients.ts
@@ -0,0 +1,18 @@
+import app from "next/app";
+import { NextApiRequest, NextApiResponse } from "next";
+import { responseSymbol } from "next/dist/server/web/spec-compliant/fetch-event";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    const url = `${process.env.AUTH0_DOMAIN}/authorize?client_id=${process.env.AUTH0_CLIENT_ID}&response_type=code&redirect_uri=${process.env.AUTH0_REDIRECTURI_PATIENTS}&audience=${process.env.AUTH0_AUDIENCE}&scope=${process.env.AUTH0_SCOPES}`;
+
+    res.setHeader("Content-Type", "application/json");
+    res.redirect(url);
+  } else {
+    res.statusCode = 405;
+    res.end();
+  }
+}
