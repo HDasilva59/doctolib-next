ea0e210e0562bc390be80e3cd24ef5e620316f38
Da silva <dasilva.hugo59200@gmail.com>
2022-04-06 15:34:48 +0200

    conflicts fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,85 +1,85 @@
-import { request } from 'http'
-import type { GetServerSideProps, NextPage } from 'next'
-import Head from 'next/head'
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import { Layout } from '../component/layout'
-import { userCategory, userId, userIdPatient } from '../src/userInfos'
-import styles from '../styles/Home.module.css'
+import { request } from "http";
+import type { GetServerSideProps, NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { Layout } from "../component/layout";
+import { userCategory, userId, userIdPatient } from "../src/userInfos";
+import styles from "../styles/Home.module.css";
 import jwt_decode from "jwt-decode";
-import { getDatabase } from '../src/database'
-import moment from 'moment'
+import { getDatabase } from "../src/database";
+import moment from "moment";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-
   let user;
   let idUser;
   let decoded: any;
 
-   const accessTokken = context.req.cookies.idTokken;
+  const accessTokken = context.req.cookies.idTokken;
 
   if (accessTokken !== undefined) {
     decoded = jwt_decode(accessTokken.toString());
     user = await userCategory(decoded.email);
-    idUser= await userId(decoded.email);
+    idUser = await userId(decoded.email);
   } else {
-    user === undefined
+    user === undefined;
   }
 
   if (user === undefined && idUser === undefined) {
     return {
-    props: {
-      category: null,
-    }
-  };
-  }else if (user === "patient" ) {
+      props: {
+        category: null,
+      },
+    };
+  } else if (user === "patient") {
     idUser = await userIdPatient(decoded.email);
     return {
-    props: {
-      category: "patient",
-      idPatient: idUser?.toString(),
-    }
-  };
+      props: {
+        category: "patient",
+        idPatient: idUser?.toString(),
+      },
+    };
   } else {
     return {
-    props: {
-      category: user,
-      userId: idUser?.toString()
-    }
-  };
+      props: {
+        category: user,
+        userId: idUser?.toString(),
+      },
+    };
   }
-
-}
-
+};
 
 export default function Home(props: any) {
-  const [arrayFutureRDV, setarrayFutureRDV] = useState([])
-  const [arrayPreviousRDV, setarrayPreviousRDV] = useState([])
-   const [arrayFavoris,setarrayFavoris] = useState([])
-  const [profile, setProfile] = useState(props.category)
-
+  const [arrayFutureRDV, setarrayFutureRDV] = useState([]);
+  const [arrayPreviousRDV, setarrayPreviousRDV] = useState([]);
+  const [arrayFavoris, setarrayFavoris] = useState([]);
+  const [profile, setProfile] = useState(props.category);
 
   async function GetRDVPatient() {
-    const dataFuture = await fetch(`/api/getFutureRDV?data=${props.idPatient}`).then((result) => result.json()).then((response) => response.dataFuture);
-    const dataPast = await fetch(`/api/getFutureRDV?data=${props.idPatient}`).then((result)=>result.json()).then((response)=>response.dataPast)
-    setarrayFutureRDV(dataFuture)
-    setarrayPreviousRDV(dataPast)
+    const dataFuture = await fetch(`/api/getFutureRDV?data=${props.idPatient}`)
+      .then((result) => result.json())
+      .then((response) => response.dataFuture);
+    const dataPast = await fetch(`/api/getFutureRDV?data=${props.idPatient}`)
+      .then((result) => result.json())
+      .then((response) => response.dataPast);
+    setarrayFutureRDV(dataFuture);
+    setarrayPreviousRDV(dataPast);
   }
   async function GetFavorite() {
-    const dataFavorite = await fetch(`/api/getFavorite?data=${props.idPatient}`).then((result) => result.json()).then((response) => response.data);
+    const dataFavorite = await fetch(`/api/getFavorite?data=${props.idPatient}`)
+      .then((result) => result.json())
+      .then((response) => response.data);
     setarrayFavoris(dataFavorite);
   }
 
-  useEffect(()=> {
-    if (profile==="patient") {
-      GetRDVPatient()
-      GetFavorite()
+  useEffect(() => {
+    if (profile === "patient") {
+      GetRDVPatient();
+      GetFavorite();
     }
-  },[])
+  }, []);
 
-
-  if (profile=== null) {
-
+  if (profile === null) {
     return (
       <Layout>
         <Head>
@@ -94,9 +94,18 @@ export default function Home(props: any) {
           <div className="container" style={{ textAlign: "center" }}>
             <form action="/doctors">
               <label>
-                <input className={styles.inputtext} type="text" name="name" placeholder="Rechercher un médecin" />
+                <input
+                  className={styles.inputtext}
+                  type="text"
+                  name="name"
+                  placeholder="Rechercher un médecin"
+                />
               </label>
-              <input className={styles.inputsubmit} type="submit" value="Submit" />
+              <input
+                className={styles.inputsubmit}
+                type="submit"
+                value="Submit"
+              />
             </form>
             <div className={styles.gridleft}>
               <Link href="#">
@@ -107,20 +116,20 @@ export default function Home(props: any) {
               </Link>
             </div>
 
-
             <div className={styles.gridright}>
               <Link href="/api/auth/login">
                 <a className={styles.card}>
                   <h2>Êtes vous un pro &rarr;</h2>
-                  <p>Connectez-vous et gagnez en confort et en temps de travail</p>
+                  <p>
+                    Connectez-vous et gagnez en confort et en temps de travail
+                  </p>
                 </a>
               </Link>
             </div>
-
           </div>
         </main>
       </Layout>
-    )
+    );
   } else if (profile === "medecin") {
     return (
       <Layout>
@@ -130,15 +139,22 @@ export default function Home(props: any) {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <main>
-          <div className='container'>
-            <Link href={`/ListPatient/${props.userId}`}><a><button>List of Patients</button></a></Link>
-            <Link href={`/Calendar/${props.userId}`}><a><button>Calendar</button></a></Link>
+          <div className="container">
+            <Link href={`/ListPatient/${props.userId}`}>
+              <a>
+                <button>List of Patients</button>
+              </a>
+            </Link>
+            <Link href={`/Calendar/${props.userId}`}>
+              <a>
+                <button>Calendar</button>
+              </a>
+            </Link>
           </div>
         </main>
       </Layout>
-    )
+    );
   } else if (profile === "patient") {
-
     return (
       <Layout>
         <main >
@@ -150,9 +166,18 @@ export default function Home(props: any) {
           <div className="container-fluid " style={{ textAlign: "center" }}>
             <form className='SearchBarPatient' action="/doctors">
               <label>
-                <input className={styles.inputtext} type="text" name="name" placeholder="Rechercher un médecin" />
+                <input
+                  className={styles.inputtext}
+                  type="text"
+                  name="name"
+                  placeholder="Rechercher un médecin"
+                />
               </label>
-              <input className={styles.inputsubmit} type="submit" value="Submit" />
+              <input
+                className={styles.inputsubmit}
+                type="submit"
+                value="Submit"
+              />
             </form>
             </div>
             </div>
@@ -179,12 +204,14 @@ export default function Home(props: any) {
                 <div className='col-4 colRDV' >
                   My Favorites doctors
                   <ul className="list-group">
-
-            {arrayFavoris.map((element:any) => {
-              return (<Link key={element._id} href={`/doctors/details?id=${element._id}`}><a><li className="list-group-item" >{element.lastName} {element.firstName}</li></a></Link>)
-            })}
-          </ul>
-
+                    {arrayFutureRDV.map((element: any) => {
+                      return (
+                        <li className="list-group-item" key={element.id}>
+                          Date : {element.date} , Heure : {element.heure}
+                        </li>
+                      );
+                    })}
+                  </ul>
                 </div>
       </div>
  </div>
@@ -193,8 +220,7 @@ export default function Home(props: any) {
 
           </div>
         </main>
-
       </Layout>
-    )
+    );
   }
-};
+}
