dc63a8ae684c134598f43db9bcbde400463555f6
mansuy maxime <mmansuy@norauto.fr>
2022-04-07 14:33:32 +0200

    maj

--- component/Pdfgenerator.tsx
+++ component/Pdfgenerator.tsx
@@ -2,15 +2,13 @@ import jsPDF from 'jspdf'
 import autoTable from 'jspdf-autotable'
 
 export default function GeneratePDF(data:any) {
-console.log(data)
+
+  console.log(JSON.parse(data.data.dataPrescriptions))
   function generate() {
     const doc = new jsPDF()
-const personne = ["maxime", "23","defefefe"]
-    autoTable(doc, {
-      head: [['Name', 'Age', 'Address']],
-      body: personne.map((element)=>{return ["toto"]})
-
-    })
+    doc.text('Hello world!',20,20);
+    doc.text('This is client-side Javascript, pumping out a PDF.',20, 30);
+doc.addPage();
     doc.save('table.pdf')
   }
 
