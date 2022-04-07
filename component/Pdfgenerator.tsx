e775153a48016afe613a32991b4a41dd68a1ec12
mansuy maxime <mmansuy@norauto.fr>
2022-04-07 14:54:01 +0200

    add prescriptions

--- component/Pdfgenerator.tsx
+++ component/Pdfgenerator.tsx
@@ -2,13 +2,18 @@ import jsPDF from 'jspdf'
 import autoTable from 'jspdf-autotable'
 
 export default function GeneratePDF(data:any) {
+  const dataPrescription = JSON.parse(data.data.dataPrescriptions);
 
-  console.log(JSON.parse(data.data.dataPrescriptions))
   function generate() {
     const doc = new jsPDF()
-    doc.text('Hello world!',20,20);
-    doc.text('This is client-side Javascript, pumping out a PDF.',20, 30);
-doc.addPage();
+    doc.text('Your Prescription', 50, 20);
+    doc.text(' ', 20, 30);
+    doc.text(' ', 20, 40);
+    doc.text('Object : Prescription ', 20, 50);
+    doc.text(' ', 20, 60);
+    doc.text(`Date : ${dataPrescription[0].date}`, 20, 70);
+    doc.text(`- ${dataPrescription[0].contenu}`, 20, 90);
+    doc.addPage();
     doc.save('table.pdf')
   }
 
