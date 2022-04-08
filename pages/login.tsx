eae8f28579974d838f88b99d6dedc7ceaea81a97
Yohan M <inuyasha59260@gmail.com>
2022-04-08 11:00:03 +0200

    CSS Page login WIP

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,3 +1,4 @@
+import { url } from "inspector";
 import Link from "next/link";
 import { Layout } from "../component/layout";
 import styles from "../styles/Home.module.css";
@@ -5,35 +6,29 @@ import styles from "../styles/Home.module.css";
 export default function Login(props: { props: any }) {
   return (
     <Layout>
-      <div className={styles.pagecontainerwelcome}>
-        <div className={styles.itemcenter}>
+
           <main className="m-auto">
             <h1 className={styles.title}  >Login</h1>
-            <div className="btn-group" role="group" aria-label="Basic example">
+            <div className="row">
+
+              <div className="col " style={{ backgroundImage: "images/backgrounddoctor1.png" ,backgroundSize: "contain", height:500, paddingTop: 50 ,borderRadius: 20,marginLeft: "1%"}}>
               <Link href="/api/auth/login">
-                <a>
-                  <button
-                    type="button"
-                    className="btn btn-secondary btn-lg btn-block"
-                  >
-                    Connexion Pro
-                  </button>
+                <a className={styles.card}  style={{ marginLeft: 250 }}>
+                    Connexion Pro &rarr;
                 </a>
               </Link>
+              </div>
+              <div className="col " style={{ backgroundColor: "blue" , backgroundSize: "contain", height:500, paddingTop: 50 ,borderRadius: 20,marginRight: "1%"}}>
               <Link href="/api/auth/loginpatients">
-                <a>
-                  <button
-                    type="button"
-                    className="btn btn-secondary btn-lg btn-block"
-                  >
-                    Connexion Patient
-                  </button>
+                <a className={styles.card} style={{ textAlign: "right",marginLeft: 250}}>
+                    Connexion Patient &rarr;
                 </a>
               </Link>
+              </div>
             </div>
+
           </main>
-        </div>
-      </div>
+
     </Layout>
   );
 }
