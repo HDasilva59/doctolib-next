fb6722365aa2caad81afbc0a191ddae80afc4f0b
Yohan M <inuyasha59260@gmail.com>
2022-04-08 15:09:21 +0200

    Page login fini, button avec bg images

--- pages/login.tsx
+++ pages/login.tsx
@@ -11,15 +11,15 @@ export default function Login(props: { props: any }) {
             <h1 className={styles.title}  >Login</h1>
             <div className="row">
 
-              <div className="col " style={{ backgroundImage: "images/backgrounddoctor1.png" ,backgroundSize: "contain", height:500, paddingTop: 50 ,borderRadius: 20,marginLeft: "1%"}}>
+              <div className="col Uno " style={{  height:550, paddingTop: 50 ,borderRadius: 20,marginRight: "50px",marginLeft:"50px"}}>
               <Link href="/api/auth/login">
-                <a className={styles.card}  style={{ marginLeft: 250 }}>
+                <a className={styles.card}  style={{ marginLeft: 222 }}>
                     Connexion Pro &rarr;
                 </a>
               </Link>
               </div>
-              <div className="col " style={{ backgroundColor: "blue" , backgroundSize: "contain", height:500, paddingTop: 50 ,borderRadius: 20,marginRight: "1%"}}>
-              <Link href="/api/auth/loginpatients">
+              <div className="col Dos " style={{  height:550, paddingTop: 50 ,borderRadius: 20,marginRight: "50px",marginLeft:"50px"}}>
+              <Link href="/api/auth/loginpatients" >
                 <a className={styles.card} style={{ textAlign: "right",marginLeft: 250}}>
                     Connexion Patient &rarr;
                 </a>
